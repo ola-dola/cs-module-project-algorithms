fe578e8685125b5a170eebd8f1e0c152dbcba243
ola-dola <oredolaolamide@gmail.com>
2020-06-17 14:17:28 +0100

    solve moving zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,18 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
 def moving_zeroes(arr):
     # Your code here
+    if 0 not in arr:
+        return arr
 
-    pass
+    new_arr = [item for item in arr if item != 0]
+    while len(new_arr) < len(arr):
+        new_arr.append(0)
+
+    return new_arr
 
 
 if __name__ == '__main__':
