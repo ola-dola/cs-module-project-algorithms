dd9829dbb81c94f087a761c7a7353c963087e78e
ola-dola <oredolaolamide@gmail.com>
2020-06-17 13:39:55 +0100

    solve single number problem

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,10 +2,20 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+
+
 def single_number(arr):
     # Your code here
+    elements = {}
+    for item in arr:
+        if item in elements:
+            elements[item] += 1
+        else:
+            elements[item] = 1
 
-    pass
+    for key, val in elements.items():
+        if val == 1:
+            return key
 
 
 if __name__ == '__main__':
